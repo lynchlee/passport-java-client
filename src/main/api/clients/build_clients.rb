bab5433dc23ad847adde9cd27e65161f94a53c96
Daniel DeGroff <daniel@inversoft.com>
2017-04-20 22:07:49 -0600

    working on client building

--- src/main/api/clients/build_clients.rb
+++ src/main/api/clients/build_clients.rb
@@ -5,7 +5,7 @@ require 'json'
 
 @api = JSON.parse(File.read('../actionUser.json'))
 @apis = [@api]
-@java_client = File.read('./java.erb')
+@java_client = File.read('./java.client')
 
 namespace = OpenStruct.new(apis: @apis)
 result = ERB.new(@java_client).result(namespace.instance_eval { binding })
