e39e4c0d21f2025283fbf8d84f077c49f3942f47
Daniel DeGroff <daniel@inversoft.com>
2017-04-20 21:59:35 -0600

    Experimenting with building the Java client from JSON

---
+++ src/main/api/clients/build_clients.rb
@@ -0,0 +1,12 @@
+require 'erb'
+require 'ostruct'
+require 'rubygems'
+require 'json'
+
+@api = JSON.parse(File.read('../actionUser.json'))
+@apis = [@api]
+@java_client = File.read('./java.erb')
+
+namespace = OpenStruct.new(apis: @apis)
+result = ERB.new(@java_client).result(namespace.instance_eval { binding })
+puts result
