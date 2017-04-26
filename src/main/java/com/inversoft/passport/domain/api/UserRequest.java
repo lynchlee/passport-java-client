77035be1016811fe9d6b776487b97a038b52ec6a
Daniel DeGroff <daniel@inversoft.com>
2017-04-26 13:05:42 -0600

    Recreate the search index instead of just deleting all of the documents, and rebuild the user mapping in case the custom data scheme has been modified.

--- src/main/java/com/inversoft/passport/domain/api/UserRequest.java
+++ src/main/java/com/inversoft/passport/domain/api/UserRequest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, Inversoft Inc., All Rights Reserved
+ * Copyright (c) 2015-2017, Inversoft Inc., All Rights Reserved
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,6 +32,12 @@ public class UserRequest {
 
   @JacksonConstructor
   public UserRequest() {
+  }
+
+  public UserRequest(User user) {
+    this.sendSetPasswordEmail = false;
+    this.skipVerification = true;
+    this.user = user;
   }
 
   public UserRequest(boolean sendSetPasswordEmail, boolean skipVerification, User user) {
