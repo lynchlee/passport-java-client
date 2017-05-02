6245a857171f6cbdec433a2b6b83698b3de4f0f2
Daniel DeGroff <daniel@inversoft.com>
2017-05-02 00:50:51 -0600

    Remove JWT from the ValidateResponse.

--- src/main/java/com/inversoft/passport/domain/api/jwt/ValidateResponse.java
+++ src/main/java/com/inversoft/passport/domain/api/jwt/ValidateResponse.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, Inversoft Inc., All Rights Reserved
+ * Copyright (c) 2016-2017, Inversoft Inc., All Rights Reserved
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,11 +24,8 @@ import org.primeframework.jwt.domain.JWT;
 public class ValidateResponse {
   public JWT jwt;
 
-  public String token;
-
-  public ValidateResponse(JWT jwt, String token) {
+  public ValidateResponse(JWT jwt) {
     this.jwt = jwt;
-    this.token = token;
   }
 
   @JacksonConstructor
