6126060908f1d82b6ee7c9a230f3395dfcc81305
Tyler Scott <tyler@inversoft.com>
2017-07-20 10:38:39 -0600

    Skeleton for new groups.

---
+++ src/main/java/com/inversoft/passport/domain/Group.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2017, Inversoft Inc., All Rights Reserved
+ */
+package com.inversoft.passport.domain;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * @author Tyler Scott
+ */
+public class Group {
+  public UUID id;
+
+  public String name;
+
+  public List<String> roles;
+
+  public Group(String name) {
+    this.name = name;
+  }
+}
