35783e35a85be7c81594b90b379eacce837487dc
Daniel DeGroff <daniel@inversoft.com>
2017-07-20 23:12:21 -0600

    Working

--- src/main/java/com/inversoft/passport/domain/Group.java
+++ src/main/java/com/inversoft/passport/domain/Group.java
@@ -4,13 +4,16 @@
 package com.inversoft.passport.domain;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.UUID;
+
+import com.inversoft.json.ToString;
 
 /**
  * @author Tyler Scott
  */
 public class Group implements Buildable<Group> {
-  public GroupData data = new GroupData();
+  public GroupData data;
 
   public UUID id;
 
@@ -28,5 +31,30 @@ public class Group implements Buildable<Group> {
   public Group(UUID id, String name) {
     this.id = id;
     this.name = name;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Group group = (Group) o;
+    return Objects.equals(data, group.data) &&
+        Objects.equals(id, group.id) &&
+        Objects.equals(name, group.name) &&
+        Objects.equals(roles, group.roles);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(data, id, name, roles);
+  }
+
+  @Override
+  public String toString() {
+    return ToString.toString(this);
   }
 }
