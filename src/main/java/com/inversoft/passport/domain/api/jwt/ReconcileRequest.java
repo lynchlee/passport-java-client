55e1b308afe0b511a20cc5f977a49e6f8f4d43a5
Daniel DeGroff <daniel@inversoft.com>
2017-10-03 02:18:16 -0600

    Fix tests, working.

--- src/main/java/com/inversoft/passport/domain/api/jwt/ReconcileRequest.java
+++ src/main/java/com/inversoft/passport/domain/api/jwt/ReconcileRequest.java
@@ -30,12 +30,14 @@ public class ReconcileRequest implements Buildable<ReconcileRequest> {
   public ReconcileRequest() {
   }
 
-  public ReconcileRequest(UUID applicationId, String encodedJWT) {
+  public ReconcileRequest(UUID identityProviderId, UUID applicationId, String encodedJWT) {
+    this.identityProviderId = identityProviderId;
     this.applicationId = applicationId;
     this.encodedJWT = encodedJWT;
   }
 
-  public ReconcileRequest(UUID applicationId, String encodedJWT, String ipAddress) {
+  public ReconcileRequest(UUID identityProviderId, UUID applicationId, String encodedJWT, String ipAddress) {
+    this.identityProviderId = identityProviderId;
     this.applicationId = applicationId;
     this.encodedJWT = encodedJWT;
     this.ipAddress = ipAddress;
